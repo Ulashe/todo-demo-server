c707d6a9e82a8a1f95bdc10bf2ccb0ea4bbe77df
Ulas <ozulashe@gmail.com>
2021-11-10 23:23:21 +0300

    Server setup completed.

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const morgan = require("morgan");
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.DB_CONNECT,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  },
+  () => console.log("connected to db!")
+);
+
+mongoose.set("debug", true);
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors({ exposedHeaders: "Content-Range,X-Content-Range" }));
+
+app.use((req, res, next) => {
+  const error = new Error("Not found!");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: { message: error.message },
+  });
+});
+
+app.listen(process.env.PORT, () => console.log("Server Up and running, port: " + process.env.PORT));
